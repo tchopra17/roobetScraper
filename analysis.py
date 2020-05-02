a9aec8d19ba6ae3b338590f49034d4f29e61c08f
Tushar Chopra <tusharchopra17@gmail.com>
2020-05-02 04:58:14 -0700

    addedd auto stats and cl

--- analysis.py
+++ analysis.py
@@ -1,6 +1,48 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv("scraped.csv")
-a = df["('Crash Point',)"].str.rstrip("x")
-a = a.astype(float)
-print(np.median(a))
+
+
+
+class analysis():
+    def __init__(self, f):
+        self.cp = "('Crash Point',)"
+        self.cl = 'consec_loss'
+        self.file = f
+        self.loss = 0
+
+    def main(self):
+        df = self.prepData(1.5)
+        cl = df.groupby(self.cl)
+        counts = cl.agg("count")
+        counts = counts.drop(counts.columns[[1,2]], axis=1)
+        print(counts)
+        print("----CL: " + str(self.loss))
+        if (self.loss != (len(counts)-1)):
+            perc = counts["Unnamed: 0.1"][self.loss+1] / counts["Unnamed: 0.1"][self.loss]
+            print("----Win Percentage: " + str((1-perc)*100 )+ "%")
+        print("_________________________________________________________")
+        #breakEven(cp)
+
+
+    def prepData(self, cash_out):
+        df = pd.read_csv(self.file)
+        a = df[self.cp].str.rstrip("x")
+        df[self.cp] = a.astype(float)
+        df[self.cl] = 0
+        for i in range(len(df[self.cp])):
+            if df[self.cp][i] < cash_out:
+                self.loss += 1
+                df[self.cl][i] = self.loss
+            else:
+                self.loss = 0
+        return df
+
+    def stats(self, column):
+        print("Median: "+ str(np.median(column)))
+# Checks if there's a point where you make money regularly
+    def breakEven(column):
+        for i in range(1, 101):
+            bet = 10
+            diff = (bet * (i/100) * np.percentile(column, i)) - ((1 - (i/100)) * bet)
+            if (diff > bet):
+                print("Percentile " + str(i) + " is: " + str(np.percentile(column, i)))
--- script.py
+++ script.py
@@ -8,27 +8,54 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-options = webdriver.FirefoxOptions()
-driver = webdriver.Chrome("/Users/tusharchopra/Desktop/Tushar/Work/Development/Websites/geckodriver", chrome_options=options)
-
+from analysis import analysis
 import time
 
-driver.get("https://roobet.com/crash")
-driver.set_window_size(1920, 1200)
-time.sleep(5)
-driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
-driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
-driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
-driver.find_element_by_tag_name('body').send_keys(Keys.ENTER)
-actions = ActionChains(driver)
-#actions.move_by_offset( 795, 718).click().perform()
-a = driver.find_element(By.CSS_SELECTOR, ".tick_2dJyV:nth-child(1)").text
-df = pd.DataFrame([[a, pd.datetime.now()]], columns=(["Crash Point"], ["datetime"]))
-df.to_csv("scraped.csv",  mode='a', header=False)
-while True:
-    b = driver.find_element(By.CSS_SELECTOR, ".tick_2dJyV:nth-child(1)").text
-    if b != a:
-        a = b
-        bdf = pd.DataFrame([[b, pd.datetime.now()]], columns=(["Crash Point"], ["datetime"]))
-        bdf.to_csv("scraped.csv", mode='a', header=False)
+
+def main():
+    driver = setup()
+    fetch(driver)
+
+
+def setup():
+    print("----- Setting Up -----")
+    options = webdriver.FirefoxOptions()
+    options.set_headless()
+    driver = webdriver.Chrome("/Users/tusharchopra/Desktop/Tushar/Work/Development/Websites/geckodriver", chrome_options=options)
+    return driver
+
+def fetch(driver):
+    print("----- Fetching -----")
+    driver.get("https://roobet.com/crash")
+    driver.set_window_size(1920, 1200)
+    time.sleep(5)
+    driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
+    driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
+    driver.find_element_by_tag_name('body').send_keys(Keys.TAB)
+    driver.find_element_by_tag_name('body').send_keys(Keys.ENTER)
+    actions = ActionChains(driver)
+    #actions.move_by_offset( 795, 718).click().perform()
+    a = driver.find_element(By.CSS_SELECTOR, ".tick_2dJyV:nth-child(1)").text
+    new_csv()
+    df = pd.DataFrame([[a, pd.datetime.now()]], columns=(["Crash Point"], ["datetime"]))
+    df.to_csv("scraped.csv",  mode='a', header=False)
+
+
+    while True:
+        b = driver.find_element(By.CSS_SELECTOR, ".tick_2dJyV:nth-child(1)").text
+        if b != a:
+            a = b
+            print("---- Last Crash: " + str(b))
+            bdf = pd.DataFrame([[b, pd.datetime.now()]], columns=(["Crash Point"], ["datetime"]))
+            bdf.to_csv("scraped.csv", mode='a', header=False)
+            anal = analysis("scraped.csv")
+            anal.main()
+
+
+
+def new_csv():
+    df = pd.read_csv('scraped.csv')
+    df.to_csv("scraped.csv", index=False)
+
+if __name__ == "__main__":
+    main()
